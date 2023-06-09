e08670243a311db25d5a61001ebf4fdf88b5cef0
Cagatay Utku <cutku@student.42heilbronn.de>
2023-06-09 02:52:55 +0200

    Readline added

--- include/minishell.h
+++ include/minishell.h
@@ -3,15 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sutku <sutku@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*   By: Cutku <cutku@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 22:53:50 by sutku             #+#    #+#             */
-/*   Updated: 2023/06/08 22:54:36 by sutku            ###   ########.fr       */
+/*   Updated: 2023/06/09 00:04:12 by Cutku            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <string.h>
+# include <termios.h>
+# include <termcap.h>
+# include <errno.h>
+# include <limits.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 
 #endif
--- src/main/main.c
+++ src/main/main.c
@@ -3,10 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sutku <sutku@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*   By: Cutku <cutku@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 22:50:44 by sutku             #+#    #+#             */
-/*   Updated: 2023/06/08 22:50:45 by sutku            ###   ########.fr       */
+/*   Updated: 2023/06/09 02:51:57 by Cutku            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../include/minishell.h"
+
+int	main(int argc, char **argv, char **env)
+{
+	char	*input;
+
+	if (argc != 1)
+	{
+		write (2, "Minishell does not take any arguments\n", 39);
+		return (1);
+	}
+	else
+	{
+		input = readline("MinisHELL$ ");
+
+		printf ("%s", input);
+		free(input);
+	}
+	return (0);
+}
